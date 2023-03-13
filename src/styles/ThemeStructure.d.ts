892dd140334e166a256947d61ff02c33c7986630
Sergio <sergiot.fontal@gmail.com>
2023-03-13 13:39:04 +0100

    Create a custom theme

---
+++ src/styles/ThemeStructure.d.ts
@@ -0,0 +1,17 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      main: string;
+      secondary: string;
+      terciary: string;
+      quaternary: string;
+    };
+    fonts: {
+      main: string;
+      secondary: string;
+      terciary: string;
+    };
+  }
+}
