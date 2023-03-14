27425447fa0473a53f6f459568e7840a69073459
Sergio <sergiot.fontal@gmail.com>
2023-03-14 19:20:06 +0100

    Test LoginPage component

---
+++ src/pages/LoginPage.test.tsx
@@ -0,0 +1,54 @@
+import { screen } from "@testing-library/react";
+import { renderWithProviders } from "../testUtils/renderWithProviders";
+import LoginPage from "./LoginPage";
+
+describe("Given a LoginPage component", () => {
+  describe("When its rendered", () => {
+    test("Then it should show a heading with text 'Sign into your account'", () => {
+      renderWithProviders(<LoginPage />);
+
+      const text = "Sign into your account";
+
+      const expectedHeading = screen.getByRole("heading", { name: text });
+
+      expect(expectedHeading).toBeInTheDocument();
+    });
+
+    test("Then it should show an input labeled 'Email'", () => {
+      renderWithProviders(<LoginPage />);
+      const label = "Email";
+
+      const expectedEmailInput = screen.getByLabelText(label);
+
+      expect(expectedEmailInput).toBeInTheDocument();
+    });
+
+    test("Then it should show an input labeled 'Password'", () => {
+      renderWithProviders(<LoginPage />);
+      const label = "Password";
+
+      const expectedPasswordInput = screen.getByLabelText(label);
+
+      expect(expectedPasswordInput).toBeInTheDocument();
+    });
+
+    test("Then it should show a button with the text 'Login!' on it", () => {
+      const textButton = "Login!";
+
+      renderWithProviders(<LoginPage />);
+
+      const expectedButton = screen.getByRole("button", { name: textButton });
+
+      expect(expectedButton).toBeInTheDocument();
+    });
+
+    test("Then it should show a link with text 'Register Now'", () => {
+      renderWithProviders(<LoginPage />);
+      const linkText = "Register Now";
+
+      const expectedText = screen.getByRole("link", { name: linkText });
+
+      expect(expectedText).toBeInTheDocument();
+    });
+  });
+});
