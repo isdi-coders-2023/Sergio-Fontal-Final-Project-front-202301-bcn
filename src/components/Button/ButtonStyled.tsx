a8dea81d8b43727396a32f11d4858f8ea51a7a2e
Sergio <sergiot.fontal@gmail.com>
2023-03-29 23:52:41 +0200

    Add border style add remove default width

--- src/components/Button/ButtonStyled.tsx
+++ src/components/Button/ButtonStyled.tsx
@@ -2,13 +2,13 @@ import styled from "styled-components";
 
 const ButtonStyled = styled.button`
   background-color: ${(props) => props.theme.colors.terciary};
+  border: solid 2px ${(props) => props.theme.colors.terciary};
   border-radius: 5px;
   color: ${(props) => props.theme.colors.main};
   font-family: "Baloo Bhai 2";
   font-size: 1.25rem;
   height: 40px;
   align-self: center;
-  width: 220px;
 
   :disabled {
     opacity: 0.75;
