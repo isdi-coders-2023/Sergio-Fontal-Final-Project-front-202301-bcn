724c99f0c9f1c2d8ba0a7f26589007d362a7ccad
Sergio <sergiot.fontal@gmail.com>
2023-04-05 22:57:44 +0200

    Test logoutUser reducer

--- src/store/features/userSlice/userSlice.test.ts
+++ src/store/features/userSlice/userSlice.test.ts
@@ -1,25 +1,45 @@
-import { UserState, UserStructure } from "../types";
-import { loginUserActionCreator, userReducer } from "./userSlice";
+import {
+  loginUserActionCreator,
+  logoutUserActionCreator,
+  userReducer,
+} from "./userSlice";
+import {
+  mockedUserInitialState,
+  mockedUserLogged,
+  mockedUserUnlogged,
+} from "../../../mocks/user";
+import { UserState } from "../types";
 
 describe("Given loginUser reducer", () => {
   describe("When it receives a user state and a loginUser action", () => {
-    test("Then it should return the same user with his property isLogged set as true", () => {
-      const user: UserStructure = {
-        username: "Pet",
-        email: "pet@petalert.com",
-        token: "woofMeowMooOink",
-      };
+    test("Then it should return the same user with his property isLogged setted to true", () => {
+      const userUnlogged: UserState = mockedUserUnlogged;
 
-      const expectedUser: UserState = {
-        username: "Pet",
-        email: "pet@petalert.com",
-        token: "woofMeowMooOink",
-        isLogged: true,
-      };
+      const expectedUser: UserState = mockedUserLogged;
 
-      const userLogged = userReducer(user, loginUserActionCreator(user));
+      const userLogged: UserState = userReducer(
+        userUnlogged,
+        loginUserActionCreator(userUnlogged)
+      );
 
       expect(userLogged).toStrictEqual(expectedUser);
+    });
+  });
+});
+
+describe("Given logoutUser reducer", () => {
+  describe("When it receives a user state and a logoutUser action", () => {
+    test("Then it should return the same user with his properties username, email and token setted to empty string and its isLogged property setted to true", () => {
+      const userLogged: UserState = mockedUserLogged;
+
+      const expectedUser: UserState = mockedUserInitialState;
+
+      const userUnlogged: UserState = userReducer(
+        userLogged,
+        logoutUserActionCreator()
+      );
+
+      expect(userUnlogged).toStrictEqual(expectedUser);
     });
   });
 });
